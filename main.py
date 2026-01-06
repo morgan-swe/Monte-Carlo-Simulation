75b7c1daf9f4beac73347dd1d2c7284c1a3a7e48
geff <geffery.addai1@ucalgary.ca>
2026-01-06 15:40:00 -0700

    create dataclass - monte carlo result class - set specific attributes

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 ## person 2 code
+
+from __future__ import annotations
+
+import math
+from dataclasses import dataclass
+from typing import Optional, Literal
+
+
+import numpy as np
+
+OptionType = Literal["call, put"]
+
+
+@dataclass(frozen=True)
+class MCResult:
+    price: float
+    stderr: float
+    ci_low: float
+    ci_high: float
+    n_sims: int
+    antithetic: bool
